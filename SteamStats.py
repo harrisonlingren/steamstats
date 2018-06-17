56363a23b80393f286f0862600da3184c51dd6d8
Harrison Lingren <harrisonlingren@gmail.com>
2018-06-16 19:25:19 -0700

    working login flow

--- SteamStats.py
+++ SteamStats.py
@@ -1,23 +1,70 @@
-from flask import Flask, request, redirect, render_template, abort
+import os, re
+from flask import Flask, request, redirect, render_template, abort, jsonify, session, g
+from flask_openid import OpenID
 
 app = Flask(__name__)
 app.config.update({
+    'SECRET_KEY': os.getenv('SECRET_KEY'),
     'SESSION_PERMANENT': True,
     'PERMANENT_SESSION_LIFETIME': 2592000,
     'DEBUG': True
 })
 
+oid = OpenID(app)
+
+
 @app.route('/')
 def index():
-    return render_template('index.jinja2')
+    print(session)
+    if 'user_id' in session:
+        #print('logged in? ' + str(session['user_id']))
+        if session['user_id'] == None:
+            return render_template('login.jinja2')
+        else:
+            return render_template('index.jinja2')
+    else:
+        return render_template('login.jinja2') 
+    
 
+# login flow
+@app.route('/login')
+@oid.loginhandler
+def login():
+    return oid.try_login('https://steamcommunity.com/openid')
+
+@oid.after_login
+def create_or_login(auth):
+    _steam_re = re.compile('steamcommunity.com/openid/id/(.*?)$')
+    print('response: ' + str(auth))
+    match = _steam_re.search(auth.identity_url)
+    g.user = {}
+    steamid = match.group(1)
+    print('user: ' + str(steamid))
+    g.user['id'] = steamid
+    session['user_id'] = g.user['id']
+    user_logged_in = True
+    return redirect('/')
+
+
+# pull logged in User from cookie
+@app.before_request
+def before_request():
+    g.user = None
+    if 'user_id' in session:
+        g.user = session['user_id']
+
+
+# logout
+@app.route('/logout')
+def logout():
+    session.pop('user_id', None)
+    return redirect(oid.get_next_url())
+
+
+# 404 handler
 @app.errorhandler(404)
 def page_not_found(e):
     return render_template('404.jinja2'), 404
-    
-@app.route('/login')
-def login():
-    return render_template('login.jinja2')
 
 if __name__ == '__main__':
     app.run()
